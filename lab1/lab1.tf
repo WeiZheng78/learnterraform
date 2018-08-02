8aa98dcdd5afd4aca97d9a268c27c33f65c99783
chrisracer79 <cparent79@gmail.com>
2018-08-01 21:59:33 -0600

    test change

--- lab1/lab1.tf
+++ lab1/lab1.tf
@@ -2,7 +2,7 @@ provider "aws" {
   access_key = "AKIAJOL4TE3XYAF6WSFA"
   secret_key = "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"
   region     = "us-east-1"
-}
+} 
 
 resource "aws_instance" "myexample" {
   ami           = "ami-b374d5a5"

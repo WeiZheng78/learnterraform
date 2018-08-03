e67150fe527e9a0602d720366b6c01d8498caf75
chrisracer79 <cparent79@gmail.com>
2018-08-03 08:16:28 -0600

    Switched to default VPC

--- lab3/compute.tf
+++ lab3/compute.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web_server" {
   ami           = "${lookup(var.webserver_amis, var.aws_region)}"
   instance_type = "t2.micro"
-  subnet_id = "${aws_subnet.web_subnet.id}"
+  subnet_id = "${aws_default_subnet.learntf_default_subnet.id}"
 
   depends_on = ["aws_s3_bucket.learntf-bins"]
 }
--- lab3/network.tf
+++ lab3/network.tf
@@ -1,9 +1,4 @@
-resource "aws_vpc" "learntf_vpc" {
-  cidr_block = "10.0.0.0/24"
-}
-
-resource "aws_subnet" "web_subnet" {
-  vpc_id = "${aws_vpc.learntf_vpc.id}"
-  cidr_block = "10.0.0.0/28"
+# Create a reference to the default AWS subnet
+resource "aws_default_subnet" "learntf_default_subnet" {
   availability_zone = "us-east-1a"
 }

f77699937e2a7493a9da1ddad1088881cc08f802
chrisracer79 <cparent79@gmail.com>
2018-08-04 08:17:25 -0600

    test

--- lab1/lab1.tf
+++ lab1/lab1.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AKIAJOL4TE3XYAF6WSFA"
-  secret_key = "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"
+  access_key = "AKIAJOL4TE3308ASDFAFXYAF6WSFA"
+  secret_key = "+PCxasdfy346KO4vpTBBguOCDN/8l4IYSn"
   region     = "us-east-1"
 } 
 

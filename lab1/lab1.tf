4db963600c468a8c9d3be9cbbdf5cfdad4feb504
chrisracer79 <cparent@veyron.dtm>
2018-08-08 09:34:31 -0600

    Updated

--- lab1/lab1.tf
+++ lab1/lab1.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-  access_key = "AKIAJOL4TE3308ASDFAFXYAF6WSFA"
-  secret_key = "+PCxasdfy346KO4vpTBBguOCDN/8l4IYSn"
+  access_key = "BLAHBLAH"
+  secret_key = "BLAHBLAH"
   region     = "us-east-1"
-} 
+}
 
 resource "aws_instance" "myexample" {
   ami           = "ami-b374d5a5"
